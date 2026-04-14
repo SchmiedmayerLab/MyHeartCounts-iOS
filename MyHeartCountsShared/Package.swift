2c722bf696b68306696efe92af64640e9999479f
Lukas Kollmer <hey@lukaskollmer.de>
2026-04-14 14:42:51 +0200

    update dependencies to fix FHIRModels version at <0.9.0

--- MyHeartCountsShared/Package.swift
+++ MyHeartCountsShared/Package.swift
@@ -21,7 +21,6 @@ var packageDeps: [Package.Dependency] = [
 #if !os(Linux)
 packageDeps += [
     .package(url: "https://github.com/StanfordSpezi/SpeziStudy.git", from: "0.1.20"),
-    .package(url: "https://github.com/StanfordSpezi/SpeziStorage.git", from: "2.1.4"), // not actually used but we need to force the version until we update SpeziStudy
     .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", from: "7.0.0")
 ]
 #endif
