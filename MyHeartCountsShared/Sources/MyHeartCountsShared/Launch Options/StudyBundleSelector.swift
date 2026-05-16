a384d1fe69fde748f59e93c234f83f59f624be00
Lukas Kollmer <hey@lukaskollmer.de>
2026-05-16 11:54:27 +0200

    study bundle loading: fallback & resilience (#162)
    # study bundle loading: fallback & resilience
    
    ## :recycle: Current situation & Problem
    issues being fixed:
    1. outdated versions of the app are unable to parse & ingest newer study
    definitions. this is ok if the app was first launched & enrolled when
    the study definition at the time was compatible with the app, but is not
    ok if there is no already-cached local study bundle available since the
    app won't be able to proceed in the onboarding
    2. if the `StudyBundleLoader.update()` function fails after previously
    having succeeded during the current run of the app, it will overwrite
    the previously-obtained StudyBundle with the error result
    
    
    ## :gear: Release Notes
    - improved resilience when loading study bundles
    
    
    ## :books: Documentation
    n/a
    
    
    ## :white_check_mark: Testing
    n/a
    
    
    ### Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md).

---
+++ MyHeartCountsShared/Sources/MyHeartCountsShared/Launch Options/StudyBundleSelector.swift
@@ -0,0 +1,59 @@
+//
+// This source file is part of the My Heart Counts iOS application based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2026 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+#if !os(Linux)
+
+public import struct Foundation.URL
+
+
+public enum StudyBundleSelector: Hashable, LaunchOptionDecodable, LaunchOptionEncodable {
+    /// The default study bundle, as available in firebase
+    case firebase
+    /// A study bundle dynamically produced by version of `MyHeartCounts-StudyDefinitions` the app was compiled against
+    case bundledWithApp
+    /// The study bundle located at the specified URL.
+    case atUrl(URL)
+    
+    public init(decodingLaunchOption context: LaunchOptionDecodingContext) throws {
+        try context.assertNumRawArgs(.equal(1))
+        switch context.rawArgs[0] {
+        case "firebase":
+            self = .firebase
+        case "bundledWithApp":
+            self = .bundledWithApp
+        default:
+            self = .atUrl(try URL(decodingLaunchOption: context))
+        }
+    }
+    
+    public func launchOptionArgs(for launchOption: LaunchOption<StudyBundleSelector>) -> [String] {
+        switch self {
+        case .firebase:
+            [launchOption.key, "firebase"]
+        case .bundledWithApp:
+            [launchOption.key, "bundledWithApp"]
+        case .atUrl(let url):
+            url.launchOptionArgs(for: LaunchOption<URL>(launchOption.key, default: url))
+        }
+    }
+}
+
+
+extension LaunchOptions {
+    /// Controls from where the app obtains its study bundle.
+    ///
+    /// Allowed values are:
+    /// - `firebase`
+    /// - `bundledWithApp`
+    /// - a `URL`, which can be either a web url (https) or a local file system url.
+    ///
+    /// See ``StudyBundleSelector`` for more information.
+    public static let studyBundleSelector = LaunchOption<StudyBundleSelector>("--studyBundle", default: .firebase)
+}
+
+#endif
