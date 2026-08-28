dc4f498fa479b235344a5bd73b5af6f8b78fc30d
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2026-08-27 22:11:06 -0700

    Convert Health Data through Grove Instead of the App's FHIR Layer

--- MyHeartCounts/MyHeartCountsDelegate.swift
+++ MyHeartCounts/MyHeartCountsDelegate.swift
@@ -63,6 +63,7 @@ final class MyHeartCountsDelegate: GroveAppDelegate {
                 ManagedFileUpload.Category.liveHealthUpload
                 ManagedFileUpload.Category.historicalHealthUpload
                 ManagedFileUpload.Category.healthDeletions
+                ManagedFileUpload.Category.clinicalAttachments
                 for sensor in SensorKit.mhcSensors {
                     ManagedFileUpload.Category(sensor)
                 }
