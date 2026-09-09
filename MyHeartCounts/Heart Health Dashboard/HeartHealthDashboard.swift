11d6d69dd9472112c03b8795c782dd88f78bc023
Lukas Kollmer <hey@lukaskollmer.de>
2026-09-09 17:18:21 +0200

    x

--- MyHeartCounts/Heart Health Dashboard/HeartHealthDashboard.swift
+++ MyHeartCounts/Heart Health Dashboard/HeartHealthDashboard.swift
@@ -176,9 +176,6 @@ struct HeartHealthDashboard: View {
                     .frame(width: 80, height: 80)
                     .padding(.top, 4)
                     .padding(.bottom, -8)
-//                Text(score.title)
-//                    .font(.footnote)
-//                    .foregroundStyle(.secondary)
                 if let timeRange = score.timeRange, score.scoreAvailable {
                     // - For e.g. Sleep, we might prefer this saying "Today, 7:00" instead of just "7:00" which it would show currently
                     //   (if today's sleep session ended at 07AM), the reason being that the user might confuse the label with meaning that they slept for 7 hours.
